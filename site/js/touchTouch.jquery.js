f2aa5bfb5d73cf3bc60219ef6fcda65588d6c8f7
Luis <luisvazquezalejo@gmail.com>
2019-04-10 23:33:05 +0200

    .

--- site/js/touchTouch.jquery.js
+++ site/js/touchTouch.jquery.js
@@ -6,36 +6,6 @@
  * @license		MIT License
  */
 
-let burger = document.getElementById('burger'),
-	 nav    = document.getElementById('main-nav'),
-	 slowmo = document.getElementById('slowmo');
-
-burger.addEventListener('click', function(e){
-	this.classList.toggle('is-open');
-	nav.classList.toggle('is-open');
-});
-
-slowmo.addEventListener('click', function(e){
-	this.classList.toggle('is-slowmo');
-});
-
-/* Onload demo - dirty timeout */
-let clickEvent = new Event('click');
-
-window.addEventListener('load', function(e) {
-	slowmo.dispatchEvent(clickEvent);
-	burger.dispatchEvent(clickEvent);
-	
-	setTimeout(function(){
-		burger.dispatchEvent(clickEvent);
-		
-		setTimeout(function(){
-			slowmo.dispatchEvent(clickEvent);
-		}, 3500);
-	}, 5500);
-});
-
-
 (function(){
 
 	/* Private variables */
@@ -321,5 +291,33 @@ window.addEventListener('load', function(e) {
 			}
 		}
 	};
+let burger = document.getElementById('burger'),
+	 nav    = document.getElementById('main-nav'),
+	 slowmo = document.getElementById('slowmo');
+
+burger.addEventListener('click', function(e){
+	this.classList.toggle('is-open');
+	nav.classList.toggle('is-open');
+});
+
+slowmo.addEventListener('click', function(e){
+	this.classList.toggle('is-slowmo');
+});
+
+/* Onload demo - dirty timeout */
+let clickEvent = new Event('click');
+
+window.addEventListener('load', function(e) {
+	slowmo.dispatchEvent(clickEvent);
+	burger.dispatchEvent(clickEvent);
+	
+	setTimeout(function(){
+		burger.dispatchEvent(clickEvent);
+		
+		setTimeout(function(){
+			slowmo.dispatchEvent(clickEvent);
+		}, 3500);
+	}, 5500);
+});
 
 })(jQuery);
